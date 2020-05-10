bbf65c50d616107862b63ab57cb1d323b71e44f3
shayelee <shaye_web@163.com>
2020-05-11 00:34:09 +0800

    chore: stash

--- bin/index.ts
+++ bin/index.ts
@@ -1,5 +1,19 @@
 #! /usr/bin/env ts-node
 
+import { resolve as pathResolve } from 'path';
+import * as program from 'commander';
 import { fileTraverse } from '../src';
 
-console.log(fileTraverse);
+const excuteDir = process.cwd();
+
+import(pathResolve(__dirname, '../package.json')).then(async pkg => {
+  program
+    .version(pkg.version, '-v, --version')
+    .parse(process.argv);
+
+  const [ path ] = program.args;
+  const targetDir = pathResolve(excuteDir, path);
+  
+  const result = await fileTraverse(targetDir);
+  console.log('result =>', result);
+});
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,6 @@
 import { promisify } from 'util';
-import { readFile as _readFile } from 'fs';
+import { readdir as _readdir, readFile as _readFile, stat } from 'fs';
 
+export const fsStat = promisify(stat);
+export const readdir = promisify(_readdir);
 export const readFile = promisify(_readFile);
